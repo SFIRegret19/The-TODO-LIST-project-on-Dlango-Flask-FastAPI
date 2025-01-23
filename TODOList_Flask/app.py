df536f515b0bf1e8332945c1e58e57a57cbfd949
SFIRegret19 <fspirin8@gmail.com>
2025-01-23 20:29:14 +0700

    Изменил функцию welcome_page (сделал вывод всех задач); Создал функцию add_task с запросом POST для добавления задачи; Создал функцию удаления задачи по id; Добавил debug в app.run

--- TODOList_Flask/app.py
+++ TODOList_Flask/app.py
@@ -1,13 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from db import *
 
 app = Flask(__name__)
 
+# Инициализация базы данных при первом запросе
+with app.app_context():
+    init_db()
+
 @app.route('/')
 def welcome_page():
-    return render_template('index.html')
+    conn = get_db_connection()
+    tasks = conn.execute('SELECT * FROM tasks ORDER BY created_at DESC').fetchall()
+    conn.close()
+    return render_template('index.html', tasks=tasks)
 
+@app.route('/add', methods=['POST'])
+def add_task():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form.get('description', default='')
+        user = request.form.get('user', default='Anonymous')
+        priority = request.form.get('priority', default='Normal')
+        category = request.form.get('category', default='Uncategorized')
+        due_date = request.form.get('due_date', default=None)
 
+        conn = get_db_connection()
+        conn.execute(
+            'INSERT INTO tasks (user, title, description, priority, category, due_date) VALUES (?, ?, ?, ?, ?, ?)',
+            (user, title, description, priority, category, due_date)
+        )
+        conn.commit()
+        conn.close()
+        return redirect(url_for('welcome_page'))
+    return render_template('add_task.html')
 
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    conn = get_db_connection()
+    conn.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('welcome_page'))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
