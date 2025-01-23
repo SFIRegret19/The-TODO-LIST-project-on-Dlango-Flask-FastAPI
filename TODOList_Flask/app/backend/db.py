197069bc616a6730c1c2f9679d0206080549dd0b
SFIRegret19 <fspirin8@gmail.com>
2025-01-24 01:32:31 +0700

    Основной файл работы с бд (создание таблиц)

---
+++ TODOList_Flask/app/backend/db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+engine = create_engine('sqlite:///database.db', echo=True)
+SessionLocal = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+
+def init_db():
+    from models.user import User
+    from models.task import Task
+    Base.metadata.create_all(bind=engine)
