677c2d5b55e06a91fec8ed528256455d4c5c60bc
SFIRegret19 <fspirin8@gmail.com>
2025-01-26 03:55:28 +0700

    Добавил Flask_login для отслеживания текущего пользователя, загружаю пользователя, зарегистрировал доп роутеры на авторизацию и изменение профиля

--- TODOList_Flask/app/app.py
+++ TODOList_Flask/app/app.py
@@ -1,22 +1,34 @@
-from flask import Flask, render_template
-from backend.db_depends import get_db
-from models.task import Task
+from flask import Flask, redirect, url_for
+from flask_login import LoginManager
 from routers.task import task_router
-from routers.auth import auth_router
+from routers.auth_registr import auth_router
+from routers.user import user_router
+from models.user import User 
+from backend.db_depends import get_db
+import os
 
 app = Flask(__name__)
+
+# Инициализация Flask-Login
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'  # Указываем маршрут для страницы логина
+login_manager.init_app(app)
+
+app.secret_key = os.urandom(24)
+
+@login_manager.user_loader
+def load_user(user_id):
+    db = next(get_db())  # Получаем текущую сессию из get_db
+    return db.query(User).get(int(user_id))  # Доступ к данным через db напрямую
 
 # Регистрируем роутеры
 app.register_blueprint(task_router)
 app.register_blueprint(auth_router)
+app.register_blueprint(user_router)
 
-# Просмотр всех задач:
 @app.route('/')
-def view_tasks():
-    db = next(get_db())
-    tasks = db.query(Task).order_by(Task.created_at.desc()).all()
-    return render_template('tasks.html', tasks=tasks)
-
+def home():
+    return redirect(url_for('auth.login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
