3a228e31bd26ebbe20cbb690eeeb1f227a56efaa
SFIRegret19 <fspirin8@gmail.com>
2025-01-25 22:35:13 +0700

    изменение полей модели под enum, добавление default значений для времени и enum, автозавершение задачи при нажатии завершить задачу, перевод enum в str и проверка категории на заглавную букву

--- TODOList_Flask/app/models/task.py
+++ TODOList_Flask/app/models/task.py
@@ -1,9 +1,8 @@
 from backend.db import Base
 from sqlalchemy import Column, ForeignKey, Integer, Text, String, Boolean, DateTime, Enum
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
+from datetime import datetime, timezone
 from enums.task_priority import Priority
-
 
 class Task(Base):
     __tablename__ = 'tasks'
@@ -12,14 +11,14 @@ class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255), nullable=False)
     content = Column(Text)
-    priority = Column(Enum(Priority), default=Priority.NOT_URGENT_NOT_IMPORTANT)
+    priority = Column(Enum (Priority, default=Priority.NOT_URGENT_NOT_IMPORTANT))
     is_completed = Column(Boolean, default=False)
     category = Column(String(50), default='Все')
     progress = Column(Integer, default=0)
     attachment = Column(String(255))
-    created_at = Column(DateTime, default=func.now())
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
     due_date = Column(DateTime)
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    updated_at = Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
     completed_at = Column(DateTime)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
     slug = Column(String, unique=True, index=True)
@@ -31,7 +30,11 @@ class Task(Base):
     def formatted_category(self):
         return self.category.capitalize() if self.category else self.category
     
+    @property
+    def priority_str(self):
+        return str(self.priority).replace("_", " ").title() # Преобразует Enum в строку
+
     # Автоматически ставим дату завершения
     def mark_completed(self):
         self.is_completed = True
-        self.completed_at = func.now() 
+        self.completed_at = datetime.now(timezone.utc)
